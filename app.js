c9fc47ccef8ad69907a43c2c30b680589bb351b9
phANTom2303 <anikavy.goenka@gmail.com>
2024-07-08 19:59:56 +0530

    implemented basic typing and screen update, no logic yet

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 let numpad = document.querySelector(".numpad");
 numpad.addEventListener('click', (numberClick) => {
-    
+    if (numberClick.target.classList.contains('numButton')) {
+        const toUpdate = numberClick.target.textContent;
+        updateScreen(toUpdate);
+    }
 });
+
+let operators = document.querySelector(".operators");
+operators.addEventListener('click', (operatorClick) => {
+    if (operatorClick.target.classList.contains('opButton')) {
+        const toUpdate = operatorClick.target.textContent;
+        updateScreen(toUpdate);
+    }
+});
+
+function updateScreen(text) {
+    let screen = document.querySelector(".screen");
+    const currentText = screen.textContent;
+    const newText = currentText + text;
+    screen.textContent = newText;
+}
+
+
+document.querySelector(".screen").textContent = ' ';
